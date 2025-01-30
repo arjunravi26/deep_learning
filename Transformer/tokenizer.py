446712ea1cc85a84f25aab3476ec380103dbe2cb
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-01-30 20:37:01 +0530

    bert tokenizer

--- Transformer/tokenizer.py
+++ Transformer/tokenizer.py
@@ -1,3 +1,5 @@
 from transformers import BertTokenizer
+from transformers import AutoTokenizer
 
-tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+bert_tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
