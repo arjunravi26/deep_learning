906f817f4a3c831b0ab3162e8734001d82e21c07
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-01-30 20:39:00 +0530

    tokens

--- Transformer/tokenizer.py
+++ Transformer/tokenizer.py
@@ -3,3 +3,8 @@ from transformers import AutoTokenizer
 
 bert_tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
 tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+tokens = tokenizer("Using a Transformer network is simple")
+print(tokens)
+
+# load model that saved on local disk
+# tokenizer.save_pretrained("directory_on_my_computer")
