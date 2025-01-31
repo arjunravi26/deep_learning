2c2114e2419737365f42dd79d2e31252ee765ade
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-01-31 19:06:51 +0530

    chaining

---
+++ deepseek r1/testing_deepsee.py
@@ -0,0 +1,37 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import ChatOllama
+from langchain_core.messages import AIMessage
+from typing import List
+from langchain_core.tools import tool
+
+llm = ChatOllama(
+    model='deepseek-r1:1.5b',
+    temperature=0.1
+)
+messages = [
+    ("system", "You are a helpful translator. Translate the user sentence to French."),
+    ("human", "I love programming."),
+]
+response1 = llm.invoke(messages)
+print(response1.content)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant that translates {input_language} to {output_language}.",
+        ),
+        ("human", "{input}"),
+    ]
+)
+
+chain = prompt | llm
+response2 = chain.invoke(
+    {
+        "input_language": "English",
+        "output_language": "German",
+        "input": "I love programming.",
+    }
+)
+print(response2.content)
